63b8556d31f1da6b224ee4f9996073f4d49b3283
Hyungsuk Kang <hskang9@gmail.com>
2017-02-19 02:42:35 +0900

    Update settings.py
    allow access for yangyi.asuscomm.com

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pe!#ivsk$of4%g$r7-ov)n64n*yeumw7b#=kbv9etz8+0idemy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','hskang9.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','hskang9.pythonanywhere.com','127.0.0.1','yangyinetwork.asuscomm.com']
 
 
 # Application definition
